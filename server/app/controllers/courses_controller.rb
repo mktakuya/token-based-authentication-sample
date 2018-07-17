91c82a39388300b26fcf5a963aed5e5e73bfd468
Takuya Mukohira <takuya.mk96@gmail.com>
2018-07-17 15:54:53 +0900

    Implement course#create

--- server/app/controllers/courses_controller.rb
+++ server/app/controllers/courses_controller.rb
@@ -6,8 +6,27 @@ class CoursesController < ApplicationController
   end
 
   def create
+    @course = Course.new(course_params)
+
+    if @course.save
+      render json: @course
+    else
+      render json: { errors: @course.errors.full_messages }
+    end
   end
 
   def update
+  end
+
+  private
+
+  def course_params
+    params.permit(
+      :name,
+      :code,
+      :credit,
+      :instructor,
+      :content
+    )
   end
 end
