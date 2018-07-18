b979109206fbaabf7e31fc2c1d82f5538152780c
Takuya Mukohira <takuya.mk96@gmail.com>
2018-07-18 11:25:40 +0900

    Add helper methods to ApplicationController

--- server/app/controllers/application_controller.rb
+++ server/app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::API
+  include ActionController::HttpAuthentication::Token::ControllerMethods
+
+  private
+
+  def authenticate!
+    authenticate_or_request_with_http_token do |token, options|
+      User.find_by(token: token).present?
+    end
+  end
+
+  def current_user
+    @current_user ||= User.find_by(token: request.headers['Authorization'].to_s.split[1])
+  end
 end
