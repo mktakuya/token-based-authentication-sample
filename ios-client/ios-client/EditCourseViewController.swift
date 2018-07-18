95e86694913cbef897d3aa8b68e145992af00079
Takuya Mukohira <takuya.mk96@gmail.com>
2018-07-18 14:54:21 +0900

    Implement Edit Course

--- ios-client/ios-client/EditCourseViewController.swift
+++ ios-client/ios-client/EditCourseViewController.swift
@@ -52,6 +52,38 @@ class EditCourseViewController: UIViewController {
     }
     
     @IBAction func tapSubmitButton(_ sender: Any) {
+        let userDefaults = UserDefaults.standard
+        guard let token = userDefaults.string(forKey: "token") else {
+            return
+        }
+        let headers = [ "Content-Type": "application/json", "Authorization": "Bearer \(token)" ]
+        let params: Parameters = [
+            "name": self.nameField.text!,
+            "code": self.codeField.text!,
+            "instructor": self.instructorField.text!,
+            "credit": self.creditField.text!,
+            "content": self.contentView.text!
+        ]
+        
+        Alamofire.request("http://localhost:3000/courses/\(course_id)", method: .patch, parameters: params, encoding: JSONEncoding.default, headers: headers).responseJSON { response in
+            guard let object = response.result.value else {
+                return
+            }
+            let json = JSON(object)
+
+            if json["errors"] == JSON.null {
+                let storyboard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
+                let next: UIViewController = (storyboard.instantiateInitialViewController())!
+                self.present(next, animated: true, completion: nil)
+            } else {
+                let errors = json["errors"].arrayValue.map { $0.stringValue }
+                let message = errors.joined(separator: "\n")
+                let alertController = UIAlertController(title: "エラー", message: message, preferredStyle: UIAlertControllerStyle.alert)
+                let okAction = UIAlertAction(title: "OK", style: UIAlertActionStyle.default)
+                alertController.addAction(okAction)
+                self.present(alertController, animated: true, completion: nil)
+            }
+        }
     }
 
     /*
