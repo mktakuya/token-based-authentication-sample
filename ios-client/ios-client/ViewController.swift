561607f353a4b0dff2ef6f144b9d49a9ad2f4e7d
Takuya Mukohira <takuya.mk96@gmail.com>
2018-07-18 12:04:50 +0900

    Implement fetching courses

--- ios-client/ios-client/ViewController.swift
+++ ios-client/ios-client/ViewController.swift
@@ -7,19 +7,37 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
 
 class ViewController: UIViewController {
+    var courses = [Course]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        Alamofire.request("http://localhost:3000/courses", method: .get).responseJSON { response in
+            guard let object = response.result.value else {
+                return
+            }
+            let json = JSON(object)
+            json.forEach { (_, data) in
+                let course = Course()
+                course.id = data["id"].intValue
+                course.name = data["name"].stringValue
+                course.code = data["code"].stringValue
+                course.credit = data["credit"].intValue
+                course.instructor = data["instructor"].stringValue
+                course.content = data["content"].stringValue
+
+                self.courses.append(course)
+            }
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
