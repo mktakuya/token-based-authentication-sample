d88e81648a9eb28c1bab4555fc95e40300eab946
Takuya Mukohira <takuya.mk96@gmail.com>
2018-07-18 11:42:59 +0900

    Add Course model

---
+++ ios-client/ios-client/Course.swift
@@ -0,0 +1,18 @@
+//
+//  Course.swift
+//  ios-client
+//
+//  Created by Takuya Mukohira on 2018/07/18.
+//  Copyright © 2018年 Takuya Mukohira. All rights reserved.
+//
+
+import Foundation
+
+class Course {
+    var id: Int?
+    var name: String?
+    var code: String?
+    var credit: Int?
+    var instructor: String?
+    var content: String?
+}
